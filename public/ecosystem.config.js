7c4a28ba731af637d3f7d41de8b1343d3ca505d6
James Singletary <jsingletary@quietprofessionalsllc.com>
2021-03-16 22:48:41 -0400

    Added a script to handle `/` route

--- public/ecosystem.config.js
+++ public/ecosystem.config.js
@@ -1,12 +1,15 @@
 module.exports = {
+  // $ pm2 [start|restart|stop|delete] ecosystem.config.js
+  
   apps:[{
-    name: 'GAVEL',
+    name: 'gavel-app',
     script: 'serve',
+    exec_mode: cluster,
     env: {
-      PM2_SERVE_PATH: '/',
+      PM2_SERVE_PATH: '.',
       PM2_SERVE_PORT: 3000,
       PM2_SERVE_SPA: 'true',
-      PM2_SERVE_HOMEPAGE: '/home/site/wwwroot/build/index.html'
+      PM2_SERVE_HOMEPAGE: './build/index.html'
     }
 
   }]
