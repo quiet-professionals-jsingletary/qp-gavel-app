9e69221ad31265a8781dde20d0ea37380752518d
James Singletary <jsingletary@quietprofessionalsllc.com>
2021-07-09 22:30:16 -0400

    Updates to `Axios` http handlers

--- src/redux/sagas/requests/security-token.js
+++ src/redux/sagas/requests/security-token.js
@@ -3,15 +3,8 @@
  *  │ |> Axios Request - Security Token   │
  *  └─────────────────────────────────────┘
 /*/
-export function setSecurityTokenRequest() {
-  return axios.request({
-    method: "get",
-    // url:  process.env.REACT_APP_API_BASE_URL + 
-    url:  process.env.REACT_APP_API_LOCAL_URL + 
-          process.env.REACT_APP_API_VERSION + 
-          "/security-token"
-  });
-}
+// Axios
+import axios from "axios";
 
 require('dotenv').config();
 
