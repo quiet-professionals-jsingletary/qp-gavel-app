d184a21becbd3cac0a17902ed6d98ec49e8160e7
James Singletary <jsingletary@quietprofessionalsllc.com>
2021-07-14 02:13:00 -0400

    Upodates to CI/CD config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   // $ pm2 [start|restart|stop|delete] ecosystem.config.js
   apps: [{
     name: "gavel-app",
-    cwd: "/home/site/wwwroot/build",
+    cwd: "/home/site/wwwroot",
     script: "serve",
     instances:4,
     exec_mode: "cluster",
@@ -10,17 +10,17 @@ module.exports = {
     ignore_watch: ["node_modules"],
     env_production: {
       NODE_ENV: "production",
-      PM2_SERVE_PATH: "/home/site/wwwroot/build",
+      PM2_SERVE_PATH: "/home/site/wwwroot",
       PM2_SERVE_PORT: 5000,
       PM2_SERVE_INCREMENT_VAR: "PORT",
-      PM2_SERVE_HOMEPAGE: "/home/site/wwwroot/build/index.html"
+      PM2_SERVE_HOMEPAGE: "./build"
     },
     env_development: {
       NODE_ENV: "development",
-      PM2_SERVE_PATH: "./build",
+      PM2_SERVE_PATH: "/home/site/wwwroot",
       PM2_SERVE_PORT: 5030,
       PM2_SERVE_INCREMENT_VAR: "PORT",
-      PM2_SERVE_HOMEPAGE: "./build/index.html"
+      PM2_SERVE_HOMEPAGE: "./build"
     },
     filter_env: ["REACT_"],
     args: "--no-daemon --spa",
